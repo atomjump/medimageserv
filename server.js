cb13b3d6e28e68f3974f38a7ee0881e0eef66942
aj5 <peter@atomjump.com>
2016-02-02 22:40:09 -0500

    includes reading filename

--- server.js
+++ server.js
@@ -1,29 +1,51 @@
-var express =   require("express");
-var multer  =   require('multer');
-var app         =   express();
-var storage =   multer.diskStorage({
-  destination: function (req, file, callback) {
-    callback(null, './uploads');
-  },
-  filename: function (req, file, callback) {
-    callback(null, file.fieldname + '-' + Date.now());
-  }
-});
-var upload = multer({ storage : storage}).single('file1'); //single('userPhoto');
+var multiparty = require('multiparty');
+var http = require('http');
+var util = require('util');
 
-app.get('/',function(req,res){
-      res.sendFile(__dirname + "/index.html");
-});
+var myfield;
 
-app.post('/api/photo',function(req,res){
-    upload(req,res,function(err) {
-        if(err) {
-            return res.end("Error uploading file:" + err);
+http.createServer(function(req, res) {
+  
+  
+  if (req.url === '/api/photo' && req.method === 'POST') {
+    // parse a file upload
+    var form = new multiparty.Form();
+
+
+
+    form.parse(req, function(err, fields, files) {
+      res.writeHead(200, {'content-type': 'text/plain'});
+      res.write('received upload:\n\n');
+      res.end(util.inspect({fields: fields, files: files}));
+      
+      if(fields) {
+        if(fields.title) {
+           myfield = fields.title[0];
+           
         }
-        res.end("File is uploaded");
+      }
+      console.log('second:' + myfield);
+      //todo move file now
+    
     });
-});
+    
+    form.on('file', function(name, file) {
+    
+        console.log(name);
+        console.log(file.path);
+        console.log("first:" + myfield);
+    });
 
-app.listen(5566,function(){
-    console.log("Working on port 5566");
-});
+    return;
+  }
+
+  // show a file upload form
+  res.writeHead(200, {'content-type': 'text/html'});
+  res.end(
+    '<form action="/api/photo" enctype="multipart/form-data" method="post">'+
+    '<input type="text" name="title"><br>'+
+    '<input type="file" name="file1" multiple="multiple"><br>'+
+    '<input type="submit" value="Upload">'+
+    '</form>'
+  );
+}).listen(5566);
