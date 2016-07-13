e944fa7aa6c504b7d0b861085d3e17d9ef97bd2c
AtomJump <peter@atomjump.com>
2016-07-13 12:28:36 +1200

    An initial directory selector (placeholder)

---
+++ bin/install.js
@@ -0,0 +1,6 @@
+/*
+  Take input on the command-line a single parameter: Windows or Unix path, and convert into a linux path.
+  Then write that path to ../config.json under backupTo: as an entry in the array.
+  
+
+*/
