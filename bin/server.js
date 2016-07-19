db5638e8d1ef2d3b4624050f2289a6b9d93198a9
AtomJump <peter@atomjump.com>
2016-07-20 09:43:12 +1200

    Nicer look

--- bin/server.js
+++ bin/server.js
@@ -336,10 +336,10 @@ function fileWalk(startDir, cb)
 function formatBytes(bytes,decimals) {
    
    console.log("Bytes: " + bytes);
-   if(bytes == 0) return '0 Bytes';
+   if(bytes == 0) return '0 B';
    var k = 1000; // or 1024 for binary
    var dm = decimals + 1 || 3;
-   var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+   var sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
    var i = Math.floor(Math.log(bytes) / Math.log(k));
    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 }
