8826835586ba50c183c5c3df451ac670e3b272ed
AtomJump <peter@atomjump.com>
2016-10-23 14:11:21 +1300

    allow checking request to see if file exists

--- bin/server.js
+++ bin/server.js
@@ -1062,7 +1062,7 @@ function handleServer(_req, _res) {
 							}
 
 							var checkFile = getFileFromUserStr(codeFile);
-							var fullCheck = parentDir + checkFile;
+							var fullCheck = parentDir + outdirPhotos + checkFile;
 							console.log("Checking file:" + fullCheck);
 							  
 							//Check file exists async
