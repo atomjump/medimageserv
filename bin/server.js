3818c2c81171e8fe3f2afd38f5aa61e5fb947dd8
AtomJump <peter@atomjump.com>
2017-08-10 16:46:00 +1200

    experimenting with an ismap serverside

--- bin/server.js
+++ bin/server.js
@@ -1288,8 +1288,8 @@ function handleServer(_req, _res) {
 								
 								
 								var replace = {
-							   	 "CUSTOMIMAGE": "yo/10-Aug-2017-09-21-45.jpg";
-							   	 "CUSTOMWOUNDIMAGE": "yo/10-Aug-2017-09-21-45.wound-view.jpg";
+							   	 "CUSTOMIMAGE": "yo/10-Aug-2017-09-21-45.jpg",
+							   	 "CUSTOMWOUNDIMAGE": "yo/10-Aug-2017-09-21-45.wound-view.jpg"
 							   };
 								
 								
