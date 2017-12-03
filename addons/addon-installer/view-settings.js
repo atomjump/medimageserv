29d1801b66165ce14350b54134c7f949cb26e019
AtomJump <peter@atomjump.com>
2017-12-03 17:17:57 +1300

    First working installer?

--- addons/addon-installer/view-settings.js
+++ addons/addon-installer/view-settings.js
@@ -12,7 +12,7 @@ var verbose = false;
 //Globals
 var mainConfigFile = __dirname + '/../../config.json';
 var mainMedImagePath = "../../photos/";
-var targetAddonsFolder = "./addons/";
+var targetAddonsFolder = "../addons/";
 var exec = require('child_process').exec;
 var glob = require('glob-fs')({ gitignore: true });
 
