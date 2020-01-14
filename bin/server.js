7687f20aca13de88bf0178e99862d69b7bd3c2d8
AtomJump <peter@atomjump.com>
2020-01-15 07:48:00 +1300

    store the values for country code etc - testing

--- bin/server.js
+++ bin/server.js
@@ -2211,6 +2211,10 @@ function handleServer(_req, _res) {
 					   var vars = queryString.split('&');
 					   for (var i = 0; i < vars.length; i++) {
 							var pair = vars[i].split('=');
+							if(pair[0][0] == "?") {
+								//Remove first char
+								pair[0] = pair[0].substr(1);
+							}
 							data[pair[0]] = decodeURIComponent(pair[1]);
 							
 					   }
