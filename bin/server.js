461569b81d805f24ae667c278f6809972303a81b
AtomJump <peter@atomjump.com>
2020-10-15 08:59:26 +1300

    handle case of problem moving file

--- bin/server.js
+++ bin/server.js
@@ -2073,7 +2073,7 @@ function handleServer(_req, _res) {
 							var msg = "Error moving file. We have removed any files, and will let the app try again.";
 							console.log(msg);
 							thisRes.writeHead(400, {'content-type': 'text/plain'});
-        					thisRes.end("Invalid request: " + msg);
+        					thisRes.end("{ 'code': 0, 'msg': '" + msg + "'}" );
 			        		//thisRes.statusCode = 400;			//Error during transmission - tell the app about it
 	  						//thisRes.end();
 	  						return;
